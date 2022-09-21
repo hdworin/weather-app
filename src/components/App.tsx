158b083f6331ddb60790804bce60a78176f6d49d
Herman Dworin <ddworin@HermansacStudio.attlocal.net>
2022-09-21 16:04:35 -0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,52 @@
+
+import React, {FC, useState} from 'react';
+import './App.css';
+import {LocationSearch} from "./LocationSearch";
+import {LocationTable} from "./LocationTable";
+import {WeatherLocation} from "../model/Weather";
+import {searchLocation, searchCoordinates} from "../services/WeatherService";
+import {ErrorAlert, WarningAlert} from "./Alerts";
+import { WeatherSummary } from './WeatherSummary';
+
+const App: FC = () => {
+  const [locations, setLocations] = useState<WeatherLocation[]>([]);
+  const [currentLocation, setCurrentLocation] = useState<WeatherLocation | null>(null);
+  const [error, setError] = useState('');
+  const [warning, setWarning] = useState('');
+
+  const resetAlerts = () => {
+    setError('');
+    setWarning('');
+  }
+
+  let addLocation = async (term: string) => {
+    resetAlerts();
+    const location = await searchLocation(term);
+
+    if (!location) {
+      setError(`No location found called '${term}'`);
+    } else if (locations.find(item => item.id === location.id)) {
+      setWarning(`Location '${term}' is already in the list.`);
+    } else {
+      // setLocations([location, ...locations]);
+      setCurrentLocation(location);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+
+      <LocationSearch onSearch={addLocation}/>
+      {/* <CoordinateSearch onSearch={addCoordinates}/> */}
+      <ErrorAlert message={error}/>
+      <WarningAlert message={warning}/>
+      {/* <LocationTable locations={locations}
+                     current={currentLocation}
+                     onSelect={location => setCurrentLocation(location)}/> */}
+      <WeatherSummary location={currentLocation}/>
+    </div>
+  );
+};
+
+export default App;
